14fa2657c3d18f9e6a1652e0cb036613d1e48c85
dabogdan <39666410+dabogdan@users.noreply.github.com>
2021-01-02 00:20:03 +0200

    counter added (intersectionObserver)

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,34 @@
 
 // You don't need any other library to run this counter
-let beginCounting = () => {
+const onscrollTag = document.getElementById('on_scroll_counter');
 
+// let statusBox = document.getElementById("statusBox");
+// let statusText = document.getElementById("statusText");
+
+function handler(entries, observer) {
+    for (entry of entries) {
+        // console.log(entry);
+
+        // onscrollTag.textContent = entry.isIntersecting;
+
+        if (entry.isIntersecting) {
+            beginCounting();
+        } else {
+            console.log('nothing is happening')
+        }
+    }
+}
+
+/* By default, invokes the handler whenever:
+   1. Any part of the target enters the viewport
+   2. The last part of the target leaves the viewport */
+
+let observer = new IntersectionObserver(handler);
+observer.observe(document.getElementById("on_scroll_counter"));
+
+
+let beginCounting = () => {
+    console.log("it works")
     const animationDuration = 2000;
     const frameDuration = 1000 / 30;
     const totalFrames = Math.round(animationDuration / frameDuration);
